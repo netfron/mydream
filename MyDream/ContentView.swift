cf4f8a0d4ac4757d52fec032d399afd7cf367d8d
dongsu <netfron@gmail.com>
2019-10-27 17:06:10 +0900

    Initial Commit

---
+++ MyDream/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyDream
+//
+//  Created by dongsu on 2019/10/27.
+//  Copyright © 2019 dongsu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
